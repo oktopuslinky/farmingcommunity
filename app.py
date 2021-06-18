57e2ca0acf9b0d64dac3585ee0d31ca174378a55
Devansh Agrawal <devagr3924@gmail.com>
2021-06-18 12:30:29 -0500

    set up farmers and index html pages

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g, jsonify
+import sqlite3, cgi, cgitb, json
 
 app = Flask(__name__)
 
+app.database='farmers.db'
+
+app.secret_key = "opfasidn43rw908c"
+
 @app.route('/')
-def index():
-    return "Hello, World!"
+def home():
+    return render_template('index.html')
+
+@app.route('/farmers')
+def farmers():
+    return render_template('farmers.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
